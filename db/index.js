30c958ba41fa9fe73af8e2f1a09191a5dea7b97c
Mohamed Farah <47758699+FarahMo24@users.noreply.github.com>
2021-06-22 18:55:39 -0700

    removed console logs and comments and fixed minor create game bug

---
+++ db/index.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const connection = new Sequelize('d6vgcnti2vi9sf', 'pahugdzzljqqma', 'b3b5950c7acf77a6cd2b92f5886c063407c73b14ddef06f0d11571a080c7987e', {
+    host: 'ec2-54-80-184-43.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+})
+
+module.exports = connection;
